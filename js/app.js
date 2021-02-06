8c84b094889e02eb0ab607b327921c5c74e534c0
Damian <damian@MacBook-Air-Damian.local>
2021-02-06 20:11:07 +0100

    fixed y scrollbar

--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@ window.onload = () => {
     }, 4000);
 
     setTimeout(function(){
-        document.body.style.overflow = "scroll";
+        document.body.style.overflowY = "scroll";
         layer.style.visibility = "hidden";
         page.style.visibility = "visible";
     }, 5500);
